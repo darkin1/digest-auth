ec59b7ca6e6d166be28d209445b6c47ca9f61dbd
dariusz <dciesielski87@gmail.com>
2016-11-10 14:50:43 +0100

    Test

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -6,4 +6,13 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
     }
+
+    public function testHelloWorld()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+
+        $this->assertEquals('Hello World', $helloWorld->hello());
+    }
+
+
 }
