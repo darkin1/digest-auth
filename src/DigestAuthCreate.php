09be67894989826c553fcf25ecb85b6f5aa28e26
Dariusz <dciesielski87@gmail.com>
2016-12-14 15:26:21 +0000

    Apply fixes from StyleCI

--- src/DigestAuthCreate.php
+++ src/DigestAuthCreate.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace Darkin1\DigestAuth;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 /**
- * Class DigestAuthCreate
- * @package App\Classes
+ * Class DigestAuthCreate.
  */
 class DigestAuthCreate
 {
-
     /**
      * DigestAuthCreate constructor.
      */
@@ -26,6 +25,6 @@ class DigestAuthCreate
     public function make(Request $request)
     {
         return response('HTTP/1.0 401 Unauthorized', 401)
-            ->header('WWW-Authenticate', 'Digest realm="' . $this->realm . '",qop="auth",nonce="' . uniqid() . '",opaque="' . md5($this->realm));
+            ->header('WWW-Authenticate', 'Digest realm="'.$this->realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($this->realm));
     }
 }
--- src/Facades/DigestAuth.php
+++ src/Facades/DigestAuth.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Darkin1\DigestAuth\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class Intercom
- * @package Darkin1\DigestAuth\Facades
+ * Class Intercom.
  */
 class DigestAuth extends Facade
 {
